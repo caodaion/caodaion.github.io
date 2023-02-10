bd4e17f09bdf970687cc89c9c35cf8bd26cd5afd
nhanntcaodaion <118254002+nhanntcaodaion@users.noreply.github.com>
2023-02-10 10:59:32 +0700

    #37 update button share (#38)
    Co-authored-by: Nguyen Trung Nhan <nhanntdl98@gmail.com>

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -5,13 +5,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { HeaderTitleComponent } from './header-title/header-title.component';
 import { HeaderSubTitleComponent } from './header-sub-title/header-sub-title.component';
-import {MatButtonModule} from "@angular/material/button";
+import { MatButtonModule } from "@angular/material/button";
 import { HeaderDescriptionComponent } from './header-description/header-description.component';
+import { ButtonShareModule } from "../button-share/button-share.module";
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
   declarations: [HeaderComponent, HeaderTitleComponent, HeaderSubTitleComponent, HeaderDescriptionComponent],
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
   exports: [HeaderComponent, HeaderTitleComponent, HeaderSubTitleComponent, HeaderDescriptionComponent],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, ButtonShareModule, MatTooltipModule]
 })
-export class HeaderModule {}
+export class HeaderModule { }
