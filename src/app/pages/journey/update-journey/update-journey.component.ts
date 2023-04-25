2d0a9f48e236e22d8a29d1ddf1102fda53921bb6
Nguyen Trung Nhan <nhanntdl98@gmail.com>
2023-04-25 14:23:31 +0700

    deploy

--- src/app/pages/journey/update-journey/update-journey.component.ts
+++ src/app/pages/journey/update-journey/update-journey.component.ts
@@ -47,6 +47,7 @@ export class UpdateJourneyComponent implements AfterViewInit {
         this.isShowQRScanner = false
         this.onShowLog()
         this.journeyLog.location = query['l']
+        this.journeyLog.location = 'Quét mã QR'
         this.checkTuGiaJourney()
       }
     })
