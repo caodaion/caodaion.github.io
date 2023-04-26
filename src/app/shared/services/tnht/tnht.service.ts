52e1752bc2f72deaf975cd35347880f6cc7e04a8
Nguyen Trung Nhan <nhanntdl98@gmail.com>
2023-04-26 14:17:09 +0700

    deploy

--- src/app/shared/services/tnht/tnht.service.ts
+++ src/app/shared/services/tnht/tnht.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable, observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { API_PATH } from '../../constants/api.constant';
-import { THANH_NGON_HIEP_TUYEN } from '../../constants/master-data/thanh-ngon-hiep-tuyen.contstant';
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +11,9 @@ export class TnhtService {
   constructor(private http: HttpClient) {}
 
   getTNHTByPath(path: any): Observable<any> {
-    return new Observable((observer) => {
-      observer.next(THANH_NGON_HIEP_TUYEN)
-    });
+    // return new Observable((observer) => {
+    //   observer.next(THANH_NGON_HIEP_TUYEN)
+    // });
+    return this.http.get(`assets/documents/thanh-ngon-hiep-tuyen/${path}.json`);
   }
 }
