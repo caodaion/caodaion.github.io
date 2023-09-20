c779e36a3ed2db165f616b61158c085c6aef6a3e
Nguyen Trung Nhan <nhanntdl98@gmail.com>
2023-09-20 13:52:52 +0700

    deploy

--- src/app/pages/calendar/tinh-tuan-cuu/tinh-tuan-cuu.component.ts
+++ src/app/pages/calendar/tinh-tuan-cuu/tinh-tuan-cuu.component.ts
@@ -351,7 +351,7 @@ export class TinhTuanCuuComponent implements OnInit, AfterViewInit {
     };
     const stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
     const encodedHeader = base64url(stringifiedHeader);
-    const data = `${item?.details?.name || 'empty'}+${item?.details?.age || 'empty'}+${item?.details?.sex || 'empty'}+${item?.details?.color || 'empty'}+${item?.details?.title || 'empty'}+${item?.details?.subTitle || 'empty'}+${item?.details?.holyName || 'empty'}+${item?.details?.province || 'empty'}+${item?.details?.distrcit || 'empty'}+${item?.details?.ward || 'empty'}+${item?.details?.address || 'empty'}`;
+    const data = `${item?.details?.name || 'empty'}+${item?.details?.age || 'empty'}+${item?.details?.sex || 'empty'}+${item?.details?.color || 'empty'}+${item?.details?.title || 'empty'}+${item?.details?.subTitle || 'empty'}+${item?.details?.holyName || 'empty'}+${item?.details?.province || 'empty'}+${item?.details?.district || 'empty'}+${item?.details?.ward || 'empty'}+${item?.details?.address || 'empty'}`;
     const stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data));
     const encodedData = base64url(stringifiedData);
     const signature = CryptoJS.HmacSHA512("caodaiondata", "caodaionkey").toString();
