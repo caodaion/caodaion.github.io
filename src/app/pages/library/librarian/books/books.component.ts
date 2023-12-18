799c29a21cc52cbecaae2038241a7832ee841b26
Nguyen Trung Nhan <nhanntdl98@gmail.com>
2023-12-18 14:39:43 +0700

    deploy

--- src/app/pages/library/librarian/books/books.component.ts
+++ src/app/pages/library/librarian/books/books.component.ts
@@ -1,5 +1,6 @@
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'librarian-books',
@@ -36,7 +37,7 @@ export class BooksComponent implements OnInit, OnChanges {
   getBooks() {
     this.books.forEach((item: any) => {
       if (!item.isStatic) {
-        item.image = `https://raw.githubusercontent.com/caodaion-library/caodaion-library.github.io/main/${item.key}/cover.png`
+        item.image = `https://raw.githubusercontent.com/caodaion-library/caodaion-library.github.io/${!environment.production ? 'dev' : 'main'}/${item.key}/cover.png${!environment.production ? '?ref=dev' : ''}`
       }
     })
     this.getForYouBooks()
