efa2ba334c98b32f113e519d8a5e3d354facfae2
Nguyen Trung Nhan <nhanntdl98@gmail.com>
2023-12-27 09:49:54 +0700

    yodate tap-so-van

--- src/app/shared/services/soan-so/soan-so.service.ts
+++ src/app/shared/services/soan-so/soan-so.service.ts
@@ -25,6 +25,14 @@ export class SoanSoService {
           lunarTime: 'TÝ',
         }
       },
+      'so-le-via-duc-ngoc-hoang-thuong-de-thoi-ngo': {
+        name: 'Sớ Lễ Vía Đức Ngọc-Hoàng Thượng Đế (Thời Ngọ)',
+        eventLunar: {
+          lunarDay: 9,
+          lunarMonth: 1,
+          lunarTime: 'NGỌ',
+        }
+      },
       'so-le-via-duc-dieu-tri-kim-mau-thoi-ty': {
         name: 'Lễ Vía Đức Diêu Trì Kim Mẫu (Thời Tý)',
         eventLunar: {
