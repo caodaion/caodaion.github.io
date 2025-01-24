84aef54ad8b35845abf832dd6749459d40641bab
Nguyễn Trung Nhân <52454131+nhanntdl@users.noreply.github.com>
2025-01-24 08:33:13 +0700

    deploy

--- src/app/pages/home/gacha/gacha.component.ts
+++ src/app/pages/home/gacha/gacha.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { GACHA_DATA } from './gacha.data';
 import html2canvas from 'html2canvas-pro';
 
@@ -7,17 +7,24 @@ import html2canvas from 'html2canvas-pro';
   templateUrl: './gacha.component.html',
   styleUrls: ['./gacha.component.scss'],
 })
-export class GachaComponent implements OnInit {
+export class GachaComponent implements OnInit, OnDestroy {
   gachaData: any = GACHA_DATA;
   selectedFrame: any;
   selectedLecture: any;
+  selectedAudio: any;
+  selectedAudioName: any;
   isAllowDownload: boolean = false;
 
   @ViewChild('gachaFrameContainer') gachaFrameContainer: any;
   @ViewChild('gachaFrameWrapper') gachaFrameWrapper: any;
   @ViewChild('lixiBox') lixiBox: any;
+  @ViewChild('audio') audio: any;
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.audio?.nativeElement?.pause();
+  }
 
   onRunGacha() {
     this.isAllowDownload = false
@@ -26,11 +33,17 @@ export class GachaComponent implements OnInit {
       return array[Math.floor(Math.random() * array.length)];
     };
     this.selectedFrame = getRandom(this.gachaData?.frames);
+    this.selectedAudio = getRandom(this.gachaData?.audios);
     this.selectedFrame.src = this.selectedFrame.src;
-    this.selectedLecture = getRandom(this.gachaData?.lectures);        
+    this.selectedLecture = getRandom(this.gachaData?.lectures);
+    this.audio.nativeElement.src = this.selectedAudio;
     setTimeout(() => {
+      this.audio?.nativeElement?.play()
+      this.selectedAudioName = this.selectedAudio?.split('/')[this.selectedAudio?.split('/')?.length - 1]
+      this.selectedAudioName = decodeURIComponent(this.selectedAudioName)
+      
       const wrapperWidth = this.gachaFrameWrapper?.nativeElement.offsetWidth;
-      const containerWidth =
+      const containerWidth = 
         this.gachaFrameContainer?.nativeElement.offsetWidth;
       this.gachaFrameContainer.nativeElement.style.transform = `translate(-50%, -50%) scale(${
         wrapperWidth / containerWidth
