050ed7cac42d0da3c1d53b891827c191a8c06ccb
Nguyen Trung Nhan <52454131+nhanntdl@users.noreply.github.com>
2025-10-09 19:55:47 +0700

    feat: add learn sync dialog and page components for data synchronization

--- src/app/pages/learn/learn-routing.module.ts
+++ src/app/pages/learn/learn-routing.module.ts
@@ -4,30 +4,35 @@ import { Learn } from './learn';
 import { Lessons } from './components/lessons/lessons';
 import { Lesson } from './components/lesson/lesson';
 import { LearnProgressComponent } from './components/learn-progress/learn-progress.component';
+import { LearnSyncPageComponent } from './components/learn-sync-page/learn-sync-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: Learn,
-    children: [
-      {
+    {
         path: '',
-        component: Lessons
-      },
-      {
-        path: 'tien-do',
-        component: LearnProgressComponent
-      },
-      {
-        path: ':slug',
-        component: Lesson
-      }
-    ]
-  }
+        component: Learn,
+        children: [
+            {
+                path: '',
+                component: Lessons
+            },
+            {
+                path: 'sync',
+                component: LearnSyncPageComponent
+            },
+            {
+                path: 'tien-do',
+                component: LearnProgressComponent
+            },
+            {
+                path: ':slug',
+                component: Lesson
+            },
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class LearnRoutingModule { }
