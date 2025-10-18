07aa5cf2ace20565bb5848b9b888343665f207b6
buikimquangloc <118284013+buikimquangloc@users.noreply.github.com>
2025-10-18 15:08:41 +0700

    Update

--- src/app/shared/pipe/highlight-pipe.ts
+++ src/app/shared/pipe/highlight-pipe.ts
@@ -1,18 +1,21 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { inject, Pipe, PipeTransform } from '@angular/core';
+import { CommonService } from '../services/common.service';
 
 @Pipe({
   name: 'highlight'
 })
 export class HighlightPipe implements PipeTransform {
 
+  private readonly commonService = inject(CommonService);
+  
   transform(value: string, searchText: string): string {
     if (!value || !searchText) return value;
 
     // Escape ký tự đặc biệt trong regex (vd: . * + ?)
-    const escaped = searchText.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    const escaped = this.commonService.generatedSlug(searchText);
     const re = new RegExp(escaped, 'gi');
 
-    return value.replace(re, (match) => `<mark>${match}</mark>`);
+    return value.replace(re, (match: string) => `<mark>${match}</mark>`);
   }
 
 }
